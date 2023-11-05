9221db1cf77a71fbc8d58e68a7728b53fe143758
Brent Ross Habawel <rosshabawel12@gmail.com>
2023-11-05 16:24:10 +0800

    added notes.js and used props and mapping

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,13 +2,25 @@ import React from "react";
 import Heading from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
+
+let number = [1, 2, 3, 4];
+
+function createNote(notes){
+    return(
+        <Note 
+            title={notes.title}
+            content={notes.content}
+        />
+    );
+}
 
 function App(){
     return(
         <div>
             <Heading />
+            {notes.map(createNote)}
             <Footer />
-            <Note />
         </div>
     );
 };
